5fbfdca9cf2cda6a8098ab0c00291d39adb1f77d
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-03-19 02:57:05 -0600

    Solo falta la estructura de los templates

--- biblioteca/biblioteca/urls.py
+++ biblioteca/biblioteca/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from gestor.views import listar_libros  # Asegúrate de importar la vista
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("gestor.urls")),  # Redirige la raíz ("/") a las URLs de "gestor"
+    path("gestor/", include("gestor.urls")),
+    path("", listar_libros),  # Esto hace que la página principal muestre los libros
 ]
 
 
